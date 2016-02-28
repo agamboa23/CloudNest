3ce2477c0328ba0b8f6b8687ee1401fd5b881c56
Alexis Gamboa <Arxos@Alexiss-MacBook-Pro.local>
2016-02-28 03:56:14 +0100

    Fix api method permissions

--- config/policies.js
+++ config/policies.js
@@ -36,6 +36,9 @@ module.exports.policies = {
   },
   InterfaceController: {
     '*':true
+  },
+  GoogleDriveController: {
+  'getResumableUploadLocation':true
   }
   /***************************************************************************
   *                                                                          *
